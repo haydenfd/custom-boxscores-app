4f31bf0f81734dcee7f6c4773870cf988b098906
Hayden D’Souza <haydenfds@gmail.com>
2023-10-18 01:04:41 -0700

    Server init + tailwind, nextui configs

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}', './public/index.html",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
